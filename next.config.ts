a36247e459ea5e63242fdfb4825457354cae1bca
sahid afridi <ms123afridi@gmail.com>
2025-07-03 08:38:35 +0000

    I want to deploy into github pages without breaking the local environmen

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 
 import type {NextConfig} from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
+const repo = 'MyUnitConvert';
+
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'export',
+  // When deploying to Github Pages, the images need to be unoptimized
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -18,6 +19,17 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Add the basePath and assetPrefix only when building for GH Pages
+  ...(isGithubActions && {
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}/`,
+  }),
 };
 
 export default nextConfig;
